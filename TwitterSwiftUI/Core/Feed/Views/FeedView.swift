b1021cb7fbdb47d6c0e4baa8051a8d41721c150d
Vladimir <motiw@icloud.com>
2023-04-27 18:27:40 +0600

    TextArea

--- TwitterSwiftUI/Core/Feed/Views/FeedView.swift
+++ TwitterSwiftUI/Core/Feed/Views/FeedView.swift
@@ -1,7 +1,30 @@
 import SwiftUI
 
 struct FeedView: View {
+    @State private var showNewTweetView = false
     var body: some View {
+        content
+            .overlay(
+                Button(action: {
+                    showNewTweetView.toggle()
+                }) {
+                    Image("tweet")
+                        .resizable()
+                        .renderingMode(.template)
+                        .foregroundColor(.white)
+                        .frame(width: 28, height: 28)
+                        .padding()
+                        .background(Color(.systemBlue))
+                        .clipShape(Circle())
+                        .padding()
+                }
+            , alignment: .bottomTrailing)
+            .fullScreenCover(isPresented: $showNewTweetView) {
+                NewTweetView()
+            }
+    }
+    
+    var content: some View {
         ScrollView {
             LazyVStack {
                 ForEach(0 ..< 25) { _ in
---
+++ TwitterSwiftUI/Core/UploadTweet/Views/NewTweetView.swift
@@ -0,0 +1,43 @@
+import SwiftUI
+
+struct NewTweetView: View {
+    @Environment(\.dismiss) var dismiss
+    @State private var caption = ""
+    var body: some View {
+        VStack {
+            HStack {
+                Button {
+                    dismiss()
+                } label: {
+                    Text("Cancel")
+                }
+                Spacer()
+                Button {
+                    dismiss()
+                } label: {
+                    Text("Tweet")
+                        .bold()
+                        .padding(.horizontal)
+                        .padding(.vertical, 8)
+                        .background(Color(.systemBlue))
+                        .foregroundColor(.white)
+                        .clipShape(Capsule())
+                }
+            }
+            HStack(alignment: .top) {
+                Circle()
+                    .frame(width: 64, height: 64)
+                TextArea("What's happening?", text: $caption)
+            }
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            Spacer()
+        }
+        .padding()
+    }
+}
+
+struct NewTweetView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewTweetView()
+    }
+}
